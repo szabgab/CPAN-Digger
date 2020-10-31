205b5edb141455a16978c6123d7dc7ea57d44c00
Gabor Szabo <gabor@szabgab.com>
2020-10-31 21:17:30 +0200

    web application showing the details

--- webapp.pl
+++ webapp.pl
@@ -6,7 +6,7 @@ use DateTime;
 use Template;
 
 use lib "$FindBin::Bin/lib";
-use CPANDigger::DB qw(db_get_every_distro);
+use CPANDigger::DB qw(db_get_every_distro db_get_distro);
 
 
 get '/' => sub ($c) {
@@ -18,17 +18,29 @@ get '/' => sub ($c) {
         distributions => $distros,
     );
 
-    my $tt = Template->new({
-        INCLUDE_PATH => File::Spec->catdir($FindBin::Bin, 'templates'),
-        INTERPOLATE  => 1,
-    }) or die "$Template::ERROR\n";
+    # my $tt = Template->new({
+    #     INCLUDE_PATH => File::Spec->catdir($FindBin::Bin, 'templates'),
+    #     INTERPOLATE  => 1,
+    # }) or die "$Template::ERROR\n";
 
-    my $html;
-    $tt->process('main.tt', \%data, \$html) or die $tt->error(), "\n";
+    # my $html;
+    # $tt->process('main.tt', \%data, \$html) or die $tt->error(), "\n";
 
 
-    $c->render(text => $html);
+    # $c->render(text => $html);
+    $c->render(template => 'index',
+        distributions => $distros,
+        );
 };
 
+#get qr'/dist/([a-zA-Z0-9-]+)' => sub ($c) {
+get '/dist/:dist' => sub ($c) {
+    my $distribution = $c->stash('dist');;
+    my $distro = db_get_distro($distribution);
+    $c->render(template => 'distribution',
+        distribution => $distribution,
+        dist => $distro,
+    );
+};
 
 app->start;

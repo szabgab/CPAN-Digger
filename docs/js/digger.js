a9835716b3ffb71ccaa08e113b3679a934919ed3
Gabor Szabo <gabor@szabgab.com>
2023-04-04 23:51:23 +0300

    enable sorting the columns

--- docs/js/digger.js
+++ docs/js/digger.js
@@ -3,7 +3,7 @@ $(document).ready(function()
     $('#sort_table').DataTable({
       "paging": false,
       "columnDefs": [
-        { "targets": [0, 2], "orderable": true },
+        { "targets": [0, 1, 2, 7], "orderable": true },
         { "targets": "_all", "orderable": false },
       ],
       "order": [[ 2, "desc" ]]

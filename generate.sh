c8e39f63211a7a04c6da584339e1ef67443a1595
Gabor Szabo <gabor@szabgab.com>
2023-05-26 17:17:23 +0300

    don't remove old site while generating new site as the system will show 404 or even 500

--- generate.sh
+++ generate.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/bash
 
 perl -Ilib bin/cpan-digger --version
-rm -rf _site/*
-mkdir -p _site/
-cp -r static/* _site/
-mkdir _site/lists
+
 #perl -Ilib bin/cpan-digger --author SZABGAB --vcs --sleep 2
 #perl -Ilib bin/cpan-digger --filename lists/demo.txt --vcs --sleep 2
 perl -Ilib bin/cpan-digger --recent 200 --vcs --sleep 2
+
+#rm -rf _site/*
+mkdir -p _site/
+cp -r static/* _site/
 perl -Ilib bin/cpan-digger --html _site/
 

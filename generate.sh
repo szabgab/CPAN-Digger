2621b6dd4408c76e9a69db3321c485e263cfc1eb
Gabor Szabo <gabor@szabgab.com>
2023-05-28 20:04:10 +0300

    update generate.sh to include everything

--- generate.sh
+++ generate.sh
@@ -2,12 +2,16 @@
 
 perl -Ilib bin/cpan-digger --version
 
-#perl -Ilib bin/cpan-digger --author SZABGAB --vcs --sleep 2
-#perl -Ilib bin/cpan-digger --filename lists/demo.txt --vcs --sleep 2
-#perl -Ilib bin/cpan-digger --recent 2 --vcs --sleep 2
+#perl -Ilib bin/cpan-digger --releases
+#perl -Ilib bin/cpan-digger --authors
 
-#rm -rf _site/*
+perl -Ilib bin/cpan-digger --recent 20
+perl -Ilib bin/cpan-digger --coverage 40000
+perl -Ilib bin/cpan-digger --meta
+perl -Ilib bin/cpan-digger --clone 40000 --force
+perl -Ilib bin/cpan-digger --metavcs
+
+
 mkdir -p _site/
 cp -r static/* _site/
 perl -Ilib bin/cpan-digger --html _site/
-

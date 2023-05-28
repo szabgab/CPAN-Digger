ece5d73b9bd7918407aa34dc3395121ff9739e04
Gabor Szabo <gabor@szabgab.com>
2023-05-28 22:31:31 +0300

    don't force cloning any more; we have already cloned everything we could

--- generate.sh
+++ generate.sh
@@ -5,10 +5,10 @@ perl -Ilib bin/cpan-digger --version
 #perl -Ilib bin/cpan-digger --releases
 #perl -Ilib bin/cpan-digger --authors
 
-perl -Ilib bin/cpan-digger --recent 20
+perl -Ilib bin/cpan-digger --recent 50
 perl -Ilib bin/cpan-digger --coverage 40000
 perl -Ilib bin/cpan-digger --meta
-perl -Ilib bin/cpan-digger --clone 40000 --force
+perl -Ilib bin/cpan-digger --clone 40000
 perl -Ilib bin/cpan-digger --metavcs
 
 

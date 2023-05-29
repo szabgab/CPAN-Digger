ebf14f716b53babcfd12742be8db952ebbdaa4e8
Gabor Szabo <gabor@szabgab.com>
2023-05-29 10:34:28 +0300

    use --pull

--- generate.sh
+++ generate.sh
@@ -8,7 +8,7 @@ perl -Ilib bin/cpan-digger --version
 perl -Ilib bin/cpan-digger --recent 50
 perl -Ilib bin/cpan-digger --coverage 40000
 perl -Ilib bin/cpan-digger --meta
-perl -Ilib bin/cpan-digger --clone 40000
+perl -Ilib bin/cpan-digger --clone 40000 --pull
 perl -Ilib bin/cpan-digger --metavcs
 
 

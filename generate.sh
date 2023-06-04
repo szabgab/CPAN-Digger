b7f066128d88245ec3a57bdc193321078c6fc5c0
Gabor Szabo <gabor@szabgab.com>
2023-06-04 16:40:43 +0300

    collect cpants in generate

--- generate.sh
+++ generate.sh
@@ -7,6 +7,7 @@ perl -Ilib bin/cpan-digger --version
 
 perl -Ilib bin/cpan-digger --recent 100
 perl -Ilib bin/cpan-digger --dashboard
+perl -Ilib bin/cpan-digger --cpants --limit 200
 perl -Ilib bin/cpan-digger --coverage 40000
 perl -Ilib bin/cpan-digger --meta
 perl -Ilib bin/cpan-digger --clone 40000 --pull

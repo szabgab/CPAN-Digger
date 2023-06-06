4e144b8b6b7d828ac8fea0522d2a35c9801fe0ae
Gabor Szabo <gabor@szabgab.com>
2023-06-06 09:46:29 +0300

    make it easy to run any command in docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,16 @@
 #!/usr/bin/bash
 
+cmd="$*"
+if [ "$cmd" == "" ]
+then
+    cmd=./generate.sh
+fi
+#echo $cmd
+
+name="cpan-digger-$(date +%s)"
+#echo $name
+
+
 docker build -t cpan-digger .
-docker run     --rm -w /opt -v$(pwd):/opt --name cpan-digger --user ubuntu cpan-digger ./generate.sh
+docker run     --rm -w /opt -v$(pwd):/opt --name $name --user ubuntu cpan-digger $cmd
 # docker run -it --rm -w /opt -v$(pwd):/opt --name cpan-digger --user ubuntu cpan-digger bash

f3dac067d65beba1d6604836828fea898366606c
Gabor Szabo <gabor@szabgab.com>
2025-12-19 23:39:33 +0200

    Revert "increase to 1000"
    This reverts commit d2b24095efe13d405a44a41e12a4ebf86fe2deea.

--- generate.sh
+++ generate.sh
@@ -6,10 +6,10 @@ perl -Ilib bin/cpan-digger --version
 #perl -Ilib bin/cpan-digger --releases
 #perl -Ilib bin/cpan-digger --authors
 
-perl -Ilib bin/cpan-digger --recent 1000
+perl -Ilib bin/cpan-digger --recent 100
 perl -Ilib bin/cpan-digger --dashboard
 perl -Ilib bin/cpan-digger --permissions
-perl -Ilib bin/cpan-digger --cpants --limit 1000
+perl -Ilib bin/cpan-digger --cpants --limit 500
 perl -Ilib bin/cpan-digger --coverage 40000
 perl -Ilib bin/cpan-digger --meta
 perl -Ilib bin/cpan-digger --clone 40000 --pull

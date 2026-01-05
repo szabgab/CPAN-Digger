dd7b318d7cedcd0118a04b39ad44283dd1931081
Gabor Szabo <gabor@szabgab.com>
2026-01-05 10:25:52 +0200

    comments and increase to 300

--- generate.sh
+++ generate.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 set -x
 
+# Just print the version number of the CPAN Digger
 perl -Ilib bin/cpan-digger --version
 
 #perl -Ilib bin/cpan-digger --releases
 #perl -Ilib bin/cpan-digger --authors
 
-perl -Ilib bin/cpan-digger --recent 100
+# We fetch that many recent releases, but only take account the ones that are marked as "latest"
+# Which is a lot lower than the number we fetched. O(n) as we send one HTTP request for each of the "latest" distributions.
+# ~ 1.5 sec for 300 entries (skipped 155)
+perl -Ilib bin/cpan-digger --recent 300
+
+# Repository clonings. Time is unrelated to number of projects we report about.
+# ~ 1 sec
 perl -Ilib bin/cpan-digger --dashboard
+
+
+# Download a single file from cpan.org
+# ~ 7 sec
 perl -Ilib bin/cpan-digger --permissions
+
+# ~ 3 min 30 sec for 500
 perl -Ilib bin/cpan-digger --cpants --limit 500
+
+
+# ~ 30 sec for 40,000
 perl -Ilib bin/cpan-digger --coverage 40000
+
+# 
 perl -Ilib bin/cpan-digger --meta
 perl -Ilib bin/cpan-digger --clone 40000 --pull
 perl -Ilib bin/cpan-digger --metavcs

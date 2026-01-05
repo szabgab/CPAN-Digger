72d5fc5e8ae634240beca1f767b50bf6414720ab
Gabor Szabo <gabor@szabgab.com>
2026-01-05 11:09:11 +0200

    add comments and increase to 500

--- generate.sh
+++ generate.sh
@@ -2,7 +2,7 @@
 set -x
 
 # Just print the version number of the CPAN Digger
-perl -Ilib bin/cpan-digger --version
+time perl -Ilib bin/cpan-digger --version
 
 #perl -Ilib bin/cpan-digger --releases
 #perl -Ilib bin/cpan-digger --authors
@@ -10,30 +10,40 @@ perl -Ilib bin/cpan-digger --version
 # We fetch that many recent releases, but only take account the ones that are marked as "latest"
 # Which is a lot lower than the number we fetched. O(n) as we send one HTTP request for each of the "latest" distributions.
 # ~ 1.5 sec for 300 entries (skipped 155)
-perl -Ilib bin/cpan-digger --recent 300
+# ~ 1.8 sec for 500 entries (skipped 270)
+time perl -Ilib bin/cpan-digger --recent 500
 
 # Repository clonings. Time is unrelated to number of projects we report about.
 # ~ 1 sec
-perl -Ilib bin/cpan-digger --dashboard
+time perl -Ilib bin/cpan-digger --dashboard
 
 
 # Download a single file from cpan.org
 # ~ 7 sec
-perl -Ilib bin/cpan-digger --permissions
+time perl -Ilib bin/cpan-digger --permissions
 
-# ~ 3 min 30 sec for 500
-perl -Ilib bin/cpan-digger --cpants --limit 500
+# ~ 3.5 min for 500
+time perl -Ilib bin/cpan-digger --cpants --limit 500
 
 
 # ~ 30 sec for 40,000
-perl -Ilib bin/cpan-digger --coverage 40000
+time perl -Ilib bin/cpan-digger --coverage 40000
 
-# 
-perl -Ilib bin/cpan-digger --meta
-perl -Ilib bin/cpan-digger --clone 40000 --pull
-perl -Ilib bin/cpan-digger --metavcs
+# 0.5 sec
+time perl -Ilib bin/cpan-digger --meta
+
+# Clone the repositories of the distribution we have up to a max of 40000 distros.
+# The actual number is impaceted by the number of "latest" distributions we got in the "--recent" request above.
+# Was (38 sec) when using full clone
+# ~22 sec now that we have shallow clone
+time perl -Ilib bin/cpan-digger --clone 40000 --pull
 
 
+# ~ 0.4 sec
+time perl -Ilib bin/cpan-digger --metavcs
+
+
+# ~ 6 sec
 mkdir -p _site/
 cp -r static/* _site/
-perl -Ilib bin/cpan-digger --html _site/
+time perl -Ilib bin/cpan-digger --html _site/

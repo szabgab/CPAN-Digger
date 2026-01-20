6636b646f66eb967d5ff3bf48a5c3b9eccd1676d
Gabor Szabo <gabor@szabgab.com>
2026-01-20 11:23:18 +0200

    move the local date handling earlier as the jquery code seems to be crashing

--- static/js/digger.js
+++ static/js/digger.js
@@ -1,19 +1,5 @@
 $(document).ready(function()
 {
-    // console.log(window.location.href);
-    let column = 3;
-    if (window.location.href.indexOf('river') > 0) {
-        column = 15;
-    }
-    $('#sort_table').DataTable({
-      "paging": false,
-      "columnDefs": [
-        { "targets": [0, 2, 3, 8, 9, 10, 11, 12, 14, 15], "orderable": true },
-        { "targets": "_all", "orderable": false },
-      ],
-      "order": [[ column, "desc" ]]
-    });
-
     function set_local_date() {
         // Assume a date format of "2021-04-13T19:00:00+03:00";    });
         // Display time in localtime of the browser.
@@ -35,4 +21,19 @@ $(document).ready(function()
         }
     }
     set_local_date();
+
+    // console.log(window.location.href);
+    let column = 3;
+    if (window.location.href.indexOf('river') > 0) {
+        column = 15;
+    }
+    $('#sort_table').DataTable({
+      "paging": false,
+      "columnDefs": [
+        { "targets": [0, 2, 3, 8, 9, 10, 11, 12, 14, 15], "orderable": true },
+        { "targets": "_all", "orderable": false },
+      ],
+      "order": [[ column, "desc" ]]
+    });
+
 });
